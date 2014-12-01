7be675f03e6f1e0fd22bb9006f6572cd8ffef83f
smovox <smovox@PC.(none)>
2014-12-01 20:28:49 +0100

    initial project version

---
+++ includes/config.php
@@ -0,0 +1,34 @@
+<?php
+error_reporting(0);
+#error_reporting(E_ALL ^ E_NOTICE);
+
+$CONF = $TMPL = array();
+
+// The MySQL credentials
+$CONF['host'] = 'localhost';
+$CONF['user'] = 'YOURDBUSER';
+$CONF['pass'] = 'YOURDBPASS';
+$CONF['name'] = 'YOURDBNAME';
+
+// The Installation URL
+$CONF['url'] = 'http://yourdomain.com';
+
+// The Notifications e-mail
+$CONF['email'] = 'notifications@yourdomain.com';
+
+// The themes directory
+$CONF['theme_path'] = 'themes';
+
+$action = array('admin'			=> 'admin',
+				'feed'			=> 'feed',
+				'settings'		=> 'settings',
+				'messages'		=> 'messages',
+				'post'			=> 'post',
+				'recover'		=> 'recover',
+				'timeline'		=> 'timeline',
+				'profile'		=> 'profile',
+				'notifications'	=> 'notifications',
+				'search'		=> 'search',
+				'page'			=> 'page'
+				);
+?>
